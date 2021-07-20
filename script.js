62f10c00d2e171bf4a6c914d48fececb8655e97a
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-20 15:57:29 +0500

    Create constructor and add book methods

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let bokkArray = [];
+let bookArray = [];
 
 function Book(title, author, pages) {
   this.title = title;
@@ -6,6 +6,39 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-function addBookToLibrary() {
-  
+let button = document.querySelector("#newbook");
+let bookname = document.querySelector("#bookName");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
+let form = document.querySelector("#form");
+
+button.addEventListener( "click", (e)=> {
+  e.preventDefault();
+  document.querySelector("#form").classList.remove("d-none")
+})
+
+function createNewBook() {
+
+}
+
+form.addEventListener('submit', addBookToLibrary)
+
+
+function addBookToLibrary(e) {
+  e.preventDefault()
+  if (bookname.value == "" || author.value == "" || pages.value == ""){
+    alert("Fill All Fields");
+  }
+  else {
+    let new_book = {};
+    console.log(new_book)
+    new_book.name = bookname.value;
+    new_book.auhtor = author.value;
+    new_book.pages = pages.value;
+    console.log(new_book);
+    bookname.push(new_book);
+    form.submit(); // Submit the form
+   form.reset();  // Reset all form data
+   return false;
+  }
 }
